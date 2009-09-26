7a021beab4ba5ba2514f59495581c3169a19d32e
Richard Soderberg <rsoderberg@gmail.com>
2009-09-25 23:58:37 -0700

    now takes suffixes and graticules

--- distance.pl
+++ distance.pl
@@ -1,18 +1,25 @@
 #!/usr/bin/perl
 
-die "Usage: $0 <distance_in_miles> <home_lat,home_long> <afar_lat1,afar_long1> ..." unless @ARGV >= 3;
+die "Usage: $0 <distance_in_miles> <home_lat,home_long> <suffix,suffix> <prefix,prefix> ..." unless @ARGV >= 4;
 
 use GIS::Distance;
 
 my $gis = GIS::Distance->new();
 
 my($max) = $ARGV[0];
+
 my($home) = $ARGV[1];
 $home =~ s/°//g;
 my(@home) = split /\s*,\s*/, $home;
-for my $afar (@ARGV[2..$#ARGV]) {
+
+my($suf) = $ARGV[2];
+$suf =~ s/°//g;
+my(@suf) = split /\s*,\s*/, $suf;
+
+for my $afar (@ARGV[3..$#ARGV]) {
     $afar =~ s/°//g;
     my(@afar) = split /\s*,\s*/, $afar;
+    $afar[$_] .= $suf[$_] for 0..$#afar;
     my($dist) = $gis->distance(@home => @afar)->miles();
-    print "$dist\t$afar\n" if $dist <= $max;
+    print "$dist\t".join(',', @afar)."\n" if $dist <= $max;
 }
